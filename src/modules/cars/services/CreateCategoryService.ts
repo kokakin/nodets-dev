c44f84220a332b4e310288baabff302d524e030e
kokakin <pedro_guerrer@hotmail.com>
2022-12-19 07:33:42 -0300

    feat: services

---
+++ src/modules/cars/services/CreateCategoryService.ts
@@ -0,0 +1,36 @@
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+
+/**
+ * [X] - Definir o tipo de retorno
+ * [X] - Alterar o retorno de erro
+ * [X] - acessar o repositório
+ */
+class CreateCategoryService {
+
+  constructor(private categoriesRepository: ICategoriesRepository) {
+
+
+  }
+
+  execute( { description, name }: IRequest): void {
+    
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+    if (categoryAlreadyExists) {
+      throw new Error("Category already exists!");
+    }
+
+    this.categoriesRepository.create({ name, description })
+  }
+
+}
+
+
+export { CreateCategoryService }
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
-import express from "express";
+import express, { request, response } from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+import { specificationsRoutes } from "./routes/specifications.routes";
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World!" });
-});
+app.use(express.json());
+
+app.use("/categories", categoriesRoutes);
+app.use("/specifications", specificationsRoutes);
 
 app.listen(6969, () => console.log("service is running!"))
