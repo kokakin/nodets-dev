e8678ee93e11f40e48f14530ecb324c16a223652
kokakin <pedro_guerrer@hotmail.com>
2023-02-09 09:18:53 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import http from 'node:http';
+
+const server = http.createServer((request, response) => {
+  
+  const { method, url } = request;
+
+  if (method === 'GET' && url === '/users') {
+    return response.end('Listagem de usuários');
+  }
+  if (method === 'POST' && url === '/users') {
+    return response.end('Criação de usuários')
+  }
+
+  return response.end("Hello World");
+
+
+});
+
+server.listen(6969);
